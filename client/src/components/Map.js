f48c9a3fa7be6273cb017d9f41440a7d7e9c46a5
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-04-04 02:14:23 -0400

    Adding in the map and configurations for the react mapgl map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,42 +1,71 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React, { useState } from 'react';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 12
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+    // using the state from the initial viewport to pass into react map gl
+    const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+    return (
+        <div className={classes.root}>
+            <ReactMapGL
+                height="calc(100vh - 64px)"
+                onViewportChange={newViewport => setViewport(newViewport)}
+                mapboxApiAccessToken="pk.eyJ1IjoibXdhcm5zbGV5IiwiYSI6ImNqYXl5NzhuazBqOWkycXFxdTBpbDV5OG8ifQ.P8swm7oARkaph-ZrhcyXLg"
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                width="100vw"
+                {...viewport}
+            >
+                <div className={classes.navigationControl}>
+                    <NavigationControl
+                        onViewportChange={newViewport =>
+                            setViewport(newViewport)
+                        }
+                    />
+                </div>
+            </ReactMapGL>
+        </div>
+    );
 };
 
 const styles = {
-  root: {
-    display: "flex"
-  },
-  rootMobile: {
-    display: "flex",
-    flexDirection: "column-reverse"
-  },
-  navigationControl: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    margin: "1em"
-  },
-  deleteIcon: {
-    color: "red"
-  },
-  popupImage: {
-    padding: "0.4em",
-    height: 200,
-    width: 200,
-    objectFit: "cover"
-  },
-  popupTab: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
-  }
+    root: {
+        display: 'flex'
+    },
+    rootMobile: {
+        display: 'flex',
+        flexDirection: 'column-reverse'
+    },
+    navigationControl: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        margin: '1em'
+    },
+    deleteIcon: {
+        color: 'red'
+    },
+    popupImage: {
+        padding: '0.4em',
+        height: 200,
+        width: 200,
+        objectFit: 'cover'
+    },
+    popupTab: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+    }
 };
 
 export default withStyles(styles)(Map);
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import withRoot from '../withRoot';
 
 import Header from '../components/Header';
+import Map from '../components/Map';
 
 const App = () => {
-    return <Header />;
+    return (
+        <Fragment>
+            <Header />
+            <Map />
+        </Fragment>
+    );
 };
 
 export default withRoot(App);
