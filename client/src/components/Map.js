544f58e320418bb0da130c8afb77752925afc4eb
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-04-04 02:24:29 -0400

    Adding in the pin and user position to the map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import React, { useEffect, useState } from 'react';
+import ReactMapGL, { Marker, NavigationControl } from 'react-map-gl';
 import { withStyles } from '@material-ui/core/styles';
+import PinIcon from './PinIcon';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+// Setting the initial viewport of the map
 const INITIAL_VIEWPORT = {
     latitude: 37.7577,
     longitude: -122.4376,
@@ -14,7 +16,25 @@ const INITIAL_VIEWPORT = {
 const Map = ({ classes }) => {
     // using the state from the initial viewport to pass into react map gl
     const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+    // Using the state again for getting the user position and setting the user position
+    const [userPosition, setUserPosition] = useState(null);
 
+    useEffect(() => {
+        getUserPosition();
+    }, []);
+
+    // Function for getting the user position when the component is mounted
+    const getUserPosition = () => {
+        // We need to check and make sure the geolocation is present on the window navigator
+        if ('geolocation' in navigator) {
+            // If there geolocation is present then we need to get the current position and set their position and viewport
+            navigator.geolocation.getCurrentPosition(position => {
+                const { latitude, longitude } = position.coords;
+                setViewport({ ...viewport, latitude, longitude });
+                setUserPosition({ latitude, longitude });
+            });
+        }
+    };
     return (
         <div className={classes.root}>
             <ReactMapGL
@@ -32,6 +52,16 @@ const Map = ({ classes }) => {
                         }
                     />
                 </div>
+                {userPosition && (
+                    <Marker
+                        latitude={userPosition.latitude}
+                        longitude={userPosition.longitude}
+                        offsetLeft={-19}
+                        offsetTop={-37}
+                    >
+                        <PinIcon color="red" size={40} />
+                    </Marker>
+                )}
             </ReactMapGL>
         </div>
     );
